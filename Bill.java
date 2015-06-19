cb18295d0c31c4a10bf8767e8a28caea489c5829
Daniel Aranda <dwarandae@unal.edu.co>
2015-06-19 08:48:37 -0500

    Added methods to Bill class accord to UML diagram

--- Bill.java
+++ Bill.java
@@ -29,6 +29,17 @@ public class Bill{
 
 	}
 
+	public long calculateTotalCost(){
+
+		int totalDiscounts = 0;
+
+		for(int i = 0; i < discounts.size(); i++)
+		
+			totalDiscount+=discounts.get(i);
+
+		return totalCost-totalDiscounts;
+
+	}
 }
 
 	
